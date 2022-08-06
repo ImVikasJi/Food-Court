4bcdf6d86bb4d1e13bb3aca8bb363c1f7230a7d3
ImVikasJi <vbhadoriya3@gmail.com>
2022-08-06 22:12:21 +0530

    Added the star Rating dependency, reflecting details in the UI

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Foods } from 'src/app/shared/models/food';
 
 @Injectable({
   providedIn: 'root'
@@ -7,16 +8,96 @@ export class FoodService {
 
   constructor() { }
 
-  getAll():string[]{
+  getAll():Foods[]{
     return[
-      '/assets/food-1.jpg',
-      '/assets/food-2.jpg',
-      '/assets/food-3.jpg',
-      '/assets/food-4.jpg',
-      '/assets/food-5.jpg',
-      '/assets/food-6.jpg',
-      '/assets/food-2.jpg',
-      '/assets/food-1.jpg'
+      {
+        id: 1,
+        name: 'Pizza Pepperoni',
+        cookTime: '10-20 minutes',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        stars: 4.5,
+        imageUrl: '/assets/food-1.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+      {
+        id: 2,
+        name: 'Meatball',
+        price: 20,
+        cookTime: '20-30 minutes',
+        favorite: true,
+        origins: ['persia', 'middle east', 'china'],
+        stars: 4.7,
+        imageUrl: '/assets/food-2.jpg',
+        tags: ['SlowFood', 'Lunch'],
+      },
+      {
+        id: 3,
+        name: 'Hamburger',
+        price: 5,
+        cookTime: '10-15 minutes',
+        favorite: false,
+        origins: ['germany', 'us'],
+        stars: 3.5,
+        imageUrl: '/assets/food-3.jpg',
+        tags: ['FastFood', 'Hamburger'],
+      },
+      {
+        id: 4,
+        name: 'Fried Potatoes',
+        price: 2,
+        cookTime: '15-20 minutes',
+        favorite: true,
+        origins: ['belgium', 'france'],
+        stars: 3.3,
+        imageUrl: '/assets/food-4.jpg',
+        tags: ['FastFood', 'Fry'],
+      },
+      {
+        id: 5,
+        name: 'Chicken Soup',
+        price: 11,
+        cookTime: '40-50 minutes',
+        favorite: false,
+        origins: ['india', 'asia'],
+        stars: 3.0,
+        imageUrl: '/assets/food-5.jpg',
+        tags: ['SlowFood', 'Soup'],
+      },
+      {
+        id: 6,
+        name: 'Vegetables Pizza',
+        price: 9,
+        cookTime: '40-50 minutes',
+        favorite: false,
+        origins: ['italy'],
+        stars: 4.0,
+        imageUrl: '/assets/food-6.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+      {
+        id: 7,
+        name: 'Club Sandwitch',
+        price: 9,
+        cookTime: '10-15 minutes',
+        favorite: false,
+        origins: ['indian','chinese'],
+        stars: 4.0,
+        imageUrl: '/assets/food-1.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+      {
+        id: 8,
+        name: 'Honey Chilli Potatoes',
+        price: 9,
+        cookTime: '20-30 minutes',
+        favorite: false,
+        origins: ['indian','italy'],
+        stars: 4.0,
+        imageUrl: '/assets/food-2.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      }
     ]
   }
 }
